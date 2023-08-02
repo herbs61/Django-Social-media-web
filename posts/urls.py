d63ed31dfb8c12a7f4b63218b190cb0d49b71aaf
Herbert Owusu <77298594+herbs61@users.noreply.github.com>
2023-08-02 15:37:14 +0000

    Adding some functionalities eg like,comment and share

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,11 @@
+from urllib.request import parse_http_list
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('create/',views.post_create,name='create'),
+    path('feed/',views.feed,name='feed'),
+    path('like/',views.like_post,name='like'),
    
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import get_object_or_404
+from .models import Post
 
 # Create your views here.
 @login_required
@@ -17,5 +18,20 @@ def post_create(request):
         form = PostCreateForm(data=request.POST)
     return render(request,'posts/create.html',{'form':form})
 
+def feed(request):
+    posts = Post.objects.all()
+    return render(request,'posts/feed.html',{'posts':posts})
+
+
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        post.liked_by.remove(request.user)
+        
+    else:
+        post.liked_by.add(request.user)
+        
 
 
